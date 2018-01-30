b61d568507363553856ee0fe63a4ad03ab596ab2
PeterRosinski <rosinski.piotr@gmail.com>
2018-01-30 14:35:02 +0100

    commit kodilla modul 4.6

---
+++ src/com/company/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+class Calculator {
+
+    public double add(double a, double b) {
+        return a+b;
+    }
+
+    public double substract(double a, double b) {
+        return a-b;
+    }
+
+}
+
+public class Main {
+
+    public static void main(String[] args) {
+        Calculator calculator = new Calculator();
+        System.out.print("5.5 + 17.8 = ");
+        System.out.println(calculator.add(5.5,17.8));
+        System.out.print("17.8 - 5.5 = ");
+        System.out.println(calculator.substract(17.8,5.5));
+    }
+}
