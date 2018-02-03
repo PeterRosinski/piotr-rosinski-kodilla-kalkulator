7494b21cf52a21d2805ec226d234f309ecc08f0a
PeterRosinski <rosinski.piotr@gmail.com>
2018-02-03 17:39:40 +0100

    20180203 Poprawki po uwagach

--- src/com/company/Calculator.java
+++ src/com/company/Calculator.java
@@ -1,6 +1,8 @@
 package com.company;
 
-class Calculator {
+import java.io.*;
+
+public class Calculator {
 
     public double add(double a, double b) {
         return a+b;
@@ -10,15 +12,11 @@ class Calculator {
         return a-b;
     }
 
-}
-
-public class Main {
-
     public static void main(String[] args) {
         Calculator calculator = new Calculator();
         System.out.print("5.5 + 17.8 = ");
-        System.out.println(calculator.add(5.5,17.8));
+        System.out.println(calculator.add(5.5, 17.8));
         System.out.print("17.8 - 5.5 = ");
-        System.out.println(calculator.substract(17.8,5.5));
+        System.out.println(calculator.substract(17.8, 5.5));
     }
 }
